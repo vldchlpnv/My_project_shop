244db1c54e97a44116174c7f0fb900416c078dfc
Vlad Chulpanov <vlad.chulpanov@icloud.com>
2024-09-04 00:13:44 +0300

    добавил модель

--- djvenv/mishp/catalog/models.py
+++ djvenv/mishp/catalog/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
-class Instruments(models.Model):
+class InstrumentsCatalog(models.Model):
 
-    title = models.CharField(max_length=100)
-    slug = models.SlugFielg()
-    model = models.CharField(max_length=100)
-    availability = models.BooleanField()
-    quantity = models.IntegerField()
-    price = models.IntegerField()
+    title = models.ForeignKey(Category, on_delete=models.CASCADE)    # фирма производитель
+    slug = models.SlugFielg(unique=True)    # слаг
+    model = models.CharField(max_length=100)    # модель инструмента
+    availability = models.BooleanField()    # наличие
+    quantity = models.IntegerField()    # количство
+    price = models.IntegerField()    # цена
 
-    
 
-# Create your models here.
+class Category(models.Model):
+
+    type_of_instruments = models.CharField(max_length=100)    # тип инструмента
+
