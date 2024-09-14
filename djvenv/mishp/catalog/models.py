2b98547a70663ce00b71635be140310e574c3a77
Vlad Chulpanov <vlad.chulpanov@icloud.com>
2024-09-14 22:29:45 +0300

    добавил постраничную разбивку + модель для разбивки на подтипы инструментов

--- djvenv/mishp/catalog/models.py
+++ djvenv/mishp/catalog/models.py
@@ -5,6 +5,9 @@ class Category(models.Model):
 
     type_of_instruments = models.CharField(max_length=100)    # тип инструмента
     slug = models.SlugField(null=True)
+    class Meta:
+
+        ordering = ['slug']
 
     def __str__(self):
         return self.type_of_instruments
@@ -19,8 +22,21 @@ class InstrumentsCatalog(models.Model):
     price = models.IntegerField()    # цена
     type_of_instruments = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)  # тип инструмента
 
+    class Meta:
+
+        ordering = ['slug']
+
     def __str__(self):
         return self.title
+
+class ShowcaseOfInstrumentst(models.Model): # Модель с типами товаров электро, бас, акустика, пластик, комбо, голова, кабинет, и т.д.
+
+    goods = models.CharField(max_length=100)
+    type_of_goods = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    slug = models.SlugField()
+
+
+
 
 
 
--- djvenv/mishp/catalog/views.py
+++ djvenv/mishp/catalog/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render
 from django.http import Http404
 from .models import Category, InstrumentsCatalog
+from django.core.paginator import Paginator, EmptyPage
+
 
 
 def details(request, id):
     '''ф.п. перекидывающая нас
     на шаблон описывающий внутрянку каждой категории'''
 
+    inst_cat = InstrumentsCatalog.objects.filter(type_of_instruments_id=id).values()  # получим queryset из словарей
+    # со всеми данными в бд по выбранным объектам.
+    # ------------------------------------------------------------------
+    paginator = Paginator(inst_cat, 5)  # создаем экземпляр класса Paginator для постраничной разбивки
+    page_number = request.GET.get('page', 1)
+    instruments_pages = paginator.page(page_number)
+    # ------------------------------------------------------------------
     instruments_catalog = InstrumentsCatalog.objects.get(id=id)  # извлекаем все оъекты из таблицы InstrumentsCatalog
-    instruments_catalog_all = InstrumentsCatalog.objects.filter(type_of_instruments_id=id)
-
-    inst_cat = InstrumentsCatalog.objects.filter(type_of_instruments_id=id).values() # получим queryset из словарей
-    # со всеми данными в бд по выбранным объектам
 
     try:
-        guitar = Category.objects.get(id=id)
-        instruments_catalog = InstrumentsCatalog.objects.get(id=id)  # извлекаем все оъекты из таблицы InstrumentsCatalog(!!!проверить на правильность!!!)
+        head_of_page = Category.objects.get(id=id)
     except Category.DoesNotExist:
         raise Http404('Post not found')
 
     return render(request,
                   'catalog/detail.html',
-                  {'guitar':guitar, 'instruments_catalog':instruments_catalog, 'instruments_catalog_all':instruments_catalog_all, 'inst_cat':inst_cat})
-
-#'instruments_catalog_all':instruments_catalog_all
+                  {'head_of_page':head_of_page, 'instruments_catalog':instruments_catalog, 'instruments_pages':instruments_pages})
