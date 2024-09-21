991971628a74fd5404a013e2197e1cdbe6b56c3a
Vlad Chulpanov <vlad.chulpanov@icloud.com>
2024-09-22 00:27:09 +0300

    вместо id сделал по slug

--- djvenv/mishp/catalog/urls.py
+++ djvenv/mishp/catalog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name = 'catalog'
 
-urlpatterns = [path('catalog/details/<int:id>/', views.details, name='details'),
-               path('catalog/<int:id>/', views.details_of_category, name='details_of_category')]# Детали о категории
+urlpatterns = [path('catalog/details/<slug:slug>/', views.details, name='details'),
+               path('catalog/<slug:slug>/', views.details_of_category, name='details_of_category')]# Детали о категории
 
+#path('catalog/details/<int:id>/', views.details, name='details')
+#path('catalog/<int:id>/', views.details_of_category, name='details_of_category')]# Детали о категории
--- djvenv/mishp/catalog/views.py
+++ djvenv/mishp/catalog/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render
 from django.http import Http404
-from .models import Category, InstrumentsCatalog
+from .models import *
 from django.core.paginator import Paginator, EmptyPage
 
 
-def details_of_category(request, id):
+def details_of_category(request, slug):
+    '''вернет типы типы инструментов бас,
+     электро, акустика и тд'''
 
-    category_of_instr = Category.objects.get(id=id) # этим запросом получим категорию инструменов ударные гитары клавиши или прочее
+    category_of_instr = Category.objects.get(slug=slug) # этим запросом получим категорию инструменов ударные гитары клавиши или прочее
     type_of_instr_in_category = category_of_instr.showcaseofinstrumentst_set.filter(type_of_instruments_showcase_id=category_of_instr) # типы инструментов в категории бас, акустика, электро и тд
 
     return render(request, 'catalog/details_of_category.html', {'type_of_instr_in_category':type_of_instr_in_category})
 
 
-def details(request, id):
+def details(request, slug):
+    '''описывает витрину с инструментами в каталоге'''
+    #goods_in_category = InstrumentsCatalog.objects.filter(type_of_goods_id=id)
+    goods_in_category_1 = ShowcaseOfInstrumentst.objects.get(slug=slug)
+    goods_in_category = goods_in_category_1.instrumentscatalog_set.all()
+    return render(request, 'catalog/detail.html', {'goods_in_category':goods_in_category})
 
-    req_2 = InstrumentsCatalog.objects.filter(type_of_goods_id=id)
-    return render(request, 'catalog/detail.html', {'req_2':req_2})
-
+#запрос:
+#c = ShowcaseOfInstrumentst.objects.get(slug='amplifier-head')
+#c_1 = c.instrumentscatalog_set.all()
