28f24b4f9a8ab57de91179728ec56e6183870336
Vlad Chulpanov <vlad.chulpanov@icloud.com>
2024-09-25 23:59:10 +0300

    добавил проверку на постраничную разбивку и ф.с.д

--- djvenv/mishp/catalog/views.py
+++ djvenv/mishp/catalog/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import *
-from django.core.paginator import Paginator, EmptyPage
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def details_of_category(request, slug):
     '''вернет типы типы инструментов бас,
      электро, акустика и тд'''
 
-    category_of_instr = Category.objects.get(slug=slug) # этим запросом получим категорию инструменов ударные гитары клавиши или прочее
+    category_of_instr = get_object_or_404(Category, slug=slug) # этим запросом получим категорию инструменов ударные гитары клавиши или прочее,
+    # заменил на функцию сокращенного доступа
     type_of_instr_in_category = category_of_instr.showcaseofinstrumentst_set.filter(type_of_instruments_showcase_id=category_of_instr) # типы инструментов в категории бас, акустика, электро и тд
 
     return render(request, 'catalog/details_of_category.html', {'type_of_instr_in_category':type_of_instr_in_category})
@@ -18,14 +19,17 @@ def details(request, slug):
     '''описывает витрину с инструментами в каталоге'''
 
     #Сюда нужно будет добавить загловки с названиями по категориям!!!
-
-    goods_in_category_1 = ShowcaseOfInstrumentst.objects.get(slug=slug)
+    goods_in_category_1 = get_object_or_404(ShowcaseOfInstrumentst, slug=slug)
     goods_in_category = goods_in_category_1.instrumentscatalog_set.all()
 
     paginator = Paginator(goods_in_category, 5) # постраничная разбивка, пока разбиваем по 5 + добавить обработку исключений
     page_number = request.GET.get('page', 1)
-    instruments_pages = paginator.page(page_number)
+    try:
+        instruments_pages = paginator.page(page_number)
+    except (EmptyPage, PageNotAnInteger):
+        instruments_pages = paginator.page(paginator.num_pages)
 
 
     return render(request, 'catalog/detail.html', {'goods_in_category':goods_in_category, 'instruments_pages':instruments_pages})
+
 
