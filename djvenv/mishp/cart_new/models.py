2a01b254cbf8fc2ef5c78081510166c086a1efd9
Vlad Chulpanov <vlad.chulpanov@icloud.com>
2024-10-27 00:29:49 +0300

    все так же корзина

--- djvenv/mishp/cart_new/models.py
+++ djvenv/mishp/cart_new/models.py
@@ -6,7 +6,6 @@ class Cart_Items(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     goods_in_cart = models.ForeignKey(InstrumentsCatalog, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
-
     def __str__(self):
         return f"{self.quantity})"
 
--- djvenv/mishp/cart_new/views.py
+++ djvenv/mishp/cart_new/views.py
@@ -5,6 +5,10 @@ from django.contrib.auth.decorators import login_required
 from .models import Cart_Items
 from catalog.models import InstrumentsCatalog
 
+from catalog.views import *
+
+#from
+
 @login_required
 def cart_view(request):
     cart_items = Cart_Items.objects.filter(user=request.user)
@@ -19,6 +23,8 @@ def cart_view(request):
 @login_required
 def cart_add(request, goods_in_cart_id):
     product = get_object_or_404(InstrumentsCatalog, id=goods_in_cart_id)
+    path_1 = product.type_of_goods.slug # вот тут вернется слаг типа инструмента бас гитара, электо гитара
+
 
     if request.method == 'POST':
         action = request.POST.get('action')
@@ -27,14 +33,16 @@ def cart_add(request, goods_in_cart_id):
             if not created:
                 cart_item.quantity += 1
                 cart_item.save()
-            return redirect('cart_view')
+            #return redirect('catalog:details', id=product.id)
+            return redirect(f'/catalog/details/{path_1}' ) #
 
         elif action == 'increase':
             cart_item, created = Cart_Items.objects.get_or_create(user=request.user, goods_in_cart=product)
+            path_1 = product.type_of_goods.slug  # вот тут вернется слаг типа инструмента бас гитара, электо гитара
             if not created:
                 cart_item.quantity += 1
                 cart_item.save()
-            return redirect('cart_view')
+            return redirect(f'/catalog/details/{path_1}' )
 
         elif action == 'decrease':
             cart_item, created = Cart_Items.objects.get_or_create(user=request.user, goods_in_cart=product)
@@ -42,7 +50,6 @@ def cart_add(request, goods_in_cart_id):
                 cart_item.quantity -= 1
                 cart_item.save()
                 if cart_item.quantity <= 0:
-                    #del_request = Cart_Items.objects.filter(id=goods_in_cart_id).delete() # разобраться почему не работает
                     cart_item.delete()
             return redirect('cart_view')
 
